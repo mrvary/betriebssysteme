92f4a44f19a70db02dedb8c904f60fbf983b334e
mrvary <felix.breitenbach@gmail.com>
2017-11-29 13:52:15 +0100

    fixed ex. 5

--- 04_exercise/5_handling_characters.c
+++ 04_exercise/5_handling_characters.c
@@ -7,7 +7,8 @@ int main(void)
   char input[255];
   bool active = true;
   char ch;
-  int markers; // ! is a marker in this case
+  int lastMarker = -1;
+  int firstMarker = -1;
 
   printf("Please enter a String: ");
   fgets(input, 255, stdin);
@@ -18,14 +19,20 @@ int main(void)
   {
     ch = input[i];
     if (ch == 33) {
-      markers++;
+      if (firstMarker == -1) {
+        firstMarker = i;
+      } else {
+        lastMarker = i;
+      }
     }
   }
   // with 1 marker = ende is last char..TODO
   for (int i = 0; i < len; ++i) {
     ch = input[i];
-    if (ch == 33) {
-      active = !active;
+    if (i == firstMarker) {
+      active = false;
+    } else if (i == lastMarker) {
+      active = true;
     } else if ( ch < 123 && ch > 96 && active) {
       ch -= 32;
     } else if ( ch < 97 && ch > 64 && active) {
