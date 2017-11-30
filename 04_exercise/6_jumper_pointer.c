57a9936db0dd447daaa599c670e7969a7cc84c58
mrvary <felix.breitenbach@gmail.com>
2017-11-30 01:28:08 +0100

    komplette funktionalitaet aus der main-methode extrahiert und ausgelagert

--- 04_exercise/6_jumper_pointer.c
+++ 04_exercise/6_jumper_pointer.c
@@ -1,32 +1,40 @@
 #import <stdio.h>
 
-int main()
+int successfulJumps(int array[], int size)
 {
   int successfulJumps = 0;
   int pointer = 0;
-  //int A[] = {2, 3, -1, 2, -1};
-  int A[] = {2, 3, -1, 2, -2};
-  int len = sizeof(A)/sizeof(A[0]);
-  int visited = len + 1;
+  int visited = size + 1;
   int tempPointer;
 
   while (1) {
     successfulJumps++;
-    tempPointer = pointer + A[pointer];
+    tempPointer = pointer + array[pointer];
 
-    if (A[pointer] == visited) {
+    if (array[pointer] == visited) {
       successfulJumps = -1;
       break;
     }
-    if (tempPointer < 0 || tempPointer > len - 1) {
+    if (tempPointer < 0 || tempPointer > size - 1) {
       break;
     }
 
-    A[pointer] = visited;
+    array[pointer] = visited;
     pointer = tempPointer;
   }
 
-  printf("Successful jumps: %d\n", successfulJumps);
+  return successfulJumps;
+}
+
+int main()
+{
+  int A[] = {2, 3, -1, 2, -1};
+  int sizeA = sizeof(A)/sizeof(A[0]);
+  int B[] = {2, 3, -1, 2, -2};
+  int sizeB = sizeof(B)/sizeof(B[0]);
+
+  printf("Successful jumps for {2, 3, -1, 2, -1}: %d\n", successfulJumps(A, sizeA));
+  printf("Successful jumps for {2, 3, -1, 2, -2}: %d\n", successfulJumps(B, sizeB));
 
   return 0;
 }
