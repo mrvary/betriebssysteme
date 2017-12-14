476e0f7f00bceed6b11a1668bc9102a353c0df97
mrvary <felix.breitenbach@gmail.com>
2017-12-14 13:48:52 +0100

    compare fractions done. started with addition

--- 05_exercise/1_fractions.c
+++ 05_exercise/1_fractions.c
@@ -7,11 +7,15 @@ typedef struct fraction {
 
 void readFraction(Fraction *fraction);
 void printFraction(Fraction *fraction);
+Fraction *addFractions(Fraction *first, Fraction *second);
+int compareFractions(Fraction *first, Fraction *second);
 
 int main() {
-    Fraction fr;
-    readFraction(&fr);
-    printFraction(&fr);
+    Fraction fr1;
+    Fraction fr2;
+    readFraction(&fr1);
+    readFraction(&fr2);
+    printf("%d\n", compareFractions(&fr1, &fr2));
     return 0;
 }
 
@@ -25,4 +29,18 @@ void readFraction(Fraction *fraction) {
 
 void printFraction(Fraction *fraction) {
     printf("---> %d/%d\n", fraction->dividend, fraction->divisor);
+}
+
+// Fraction *addFractions(Fraction *first, Fraction *second) {
+
+// }
+
+int compareFractions(Fraction *first, Fraction *second) {
+    int equal;
+    if (first->dividend == second->dividend && first->divisor == second->divisor) {
+        equal = 0;
+    } else {
+        equal = 1;
+    }
+    return equal;
 }
