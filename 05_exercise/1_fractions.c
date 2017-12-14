81dcda85fb640e0bca93babec22ab6a8fc4ca3d6
mrvary <felix.breitenbach@gmail.com>
2017-12-14 13:33:10 +0100

    read fraction done

--- 05_exercise/1_fractions.c
+++ 05_exercise/1_fractions.c
@@ -1,9 +1,23 @@
-//
-//  1_fractions.c
-//  
-//
-//  Created by Felix Breitenbach on 14.12.17.
-//
-//
-
 #include <stdio.h>
+
+typedef struct fraction {
+    int divisor;
+    int dividend;
+} Fraction;
+
+void readFraction(Fraction *fraction);
+
+int main() {
+    Fraction fr;
+    readFraction(&fr);
+    printf("Result: %d/%d\n", fr.dividend, fr.divisor);
+    return 0;
+}
+
+void readFraction(Fraction *fraction) {
+    printf("Please enter a fraction.\nDividend: ");
+    scanf("%d", &(fraction->dividend));
+
+    printf("Divisor: ");
+    scanf("%d", &(fraction->divisor));
+}
