5462f858a2a76d75a24a8d400a23846a7bd05a94
mrvary <felix.breitenbach@gmail.com>
2017-12-20 15:05:59 +0100

    1 done

--- 06_exercise/1_isbn.c
+++ 06_exercise/1_isbn.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-int checkIsbn(char* c);
+int checkIsbn(char *c);
+int checkISBN(char *c);
 
 int main() {
 
-    printf("valid: %d\n", checkIsbn("0306406158"));
-    printf("invalid: %d\n", checkIsbn("0306406157"));
+    printf("invalid: %d\n", checkIsbn("030----6406157"));
+    printf("valid: %d\n", checkIsbn("030--640-61-58"));
+    printf("invalid: %d\n", checkIsbn("0-30-64--06152"));
+    printf("invalid: %d\n", checkIsbn("030640-6-1-5-3"));
+
     return 0;
 }
 
 int checkIsbn(char *c) {
 
     int sum;
-    for (int i; i < 10; ++i) {
-        sum += (*c) - 48;
+    for (int i; i < 14; ++i) {
+        if (*c != '-') {
+            sum += (*c) - 48;
+        }
         c++;
     }
-
-    return !((sum % 11) == 0);
+    return (sum % 11) != 0;
 }
