d5b32c5b53c7097663c24230701ff93ceff6e3a2
mrvary <felix.breitenbach@gmail.com>
2017-12-22 02:19:43 +0100

    next task done..

--- 06_exercise/4_string_differences.c
+++ 06_exercise/4_string_differences.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+int countDiffs( char *text1, char *text2 );
+
 int main() {
 
-    return 0;
+  printf("%d\n", countDiffs("Test", "Teste")); //-1
+  printf("%d\n", countDiffs("Test", "Test")); //0
+  printf("%d\n", countDiffs("Hello", "HEllo")); //1
+  printf("%d\n", countDiffs("World", "workd")); //2
+
+  return 0;
+}
+
+int countDiffs( char *text1, char *text2 ) {
+
+  int result = 0;
+  char *current1 = text1;
+  char *current2 = text2;
+
+  while (*current1 != '\0' && *current2 != '\0') {
+    if ( *current1 != *current2 ) {
+      result++;
+    }
+    current1++;
+    current2++;
+  }
+
+  if (*current1 != '\0' || *current2 != '\0') {
+    result = -1;
+  }
+
+  return result;
 }
